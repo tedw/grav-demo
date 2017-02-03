dc2d2b01d3a8052118ab21e77d538c89075dfbd0
Ted Whitehead <ted.whitehead@threespot.com>
2017-02-02 20:55:48 -0500

    Add user account

---
+++ cache/compiled/files/0b3c2f166b31682080acc5a6962d0104.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/twhitehead/Sites/grav-admin/user/accounts/tedw.yaml',
+    'modified' => 1486084462,
+    'data' => [
+        'email' => 'tedwhitehead@gmail.com',
+        'fullname' => 'Ted Whitehead',
+        'title' => 'Admin',
+        'state' => 'enabled',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$qbskU9kGu7NfvYQ6aPol0eVjb8Tkl3nLeEikfLJzeIQgWBSTBj7Z2'
+    ]
+];
